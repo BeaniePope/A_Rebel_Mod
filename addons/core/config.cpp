230c702ad42d99309b7037cf859e06889b16d282
GregGregGregGreg1234 <129117638+GregGregGregGreg1234@users.noreply.github.com>
2024-01-21 06:21:00 +0000

    a config

---
+++ addons/core/config.cpp
@@ -0,0 +1,78 @@
+class CfgPatches
+{
+    class ARMCore
+    {
+        author= "ARM mod team"
+        requiredAddos[]=
+        {
+
+        };
+        weapons[]=
+        {
+
+        };
+        units[]=
+        {
+
+        };
+    };
+;}
+class CfgFactionClasses
+{
+	class ARM_Faction
+	{
+		displayName="Rebel alliance";
+	};
+};
+class CfgEditorCategories
+{
+	class ARM_Props
+	{
+		displayName="Rebel Alliance. Supplies";
+	};
+};
+class CfgEditorSubcategories
+{
+	class ARM_pilot
+	{
+		displayName="Pilots";
+	};
+	class ARM_inf
+	{
+		displayName="Infantry";
+	};
+	class ARM_Clone_Aviator
+	{
+		displayname="Flybois"
+	};
+	class ARM_aircraft
+	{
+		displayName="Aircraft";
+	};
+	class ARM_Mechanized
+	{
+		displayName="Armoured";
+	};
+	class ARM_Arty
+	{
+		displayName="Artillery";
+	};
+	class ARM_Resupply
+	{
+		displayName="Resupply";
+		priority=1;
+	};
+	class ARM_Turret
+	{
+		displayName="Turret";
+		priority=1;
+	};
+};
+class CfgWeapons
+{
+
+};
+class CfgVehicles
+{
+
+};
