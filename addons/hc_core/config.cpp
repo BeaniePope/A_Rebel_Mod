a56f50b17eb996974a3daa9824bc91f7422f7240
BeaniePope <Swifter11110@gmail.com>
2024-07-16 03:54:57 -0400

    Quick rewrite of naming scheme

---
+++ addons/hc_core/config.cpp
@@ -0,0 +1,53 @@
+class CfgPatches
+{
+    class hc_core
+    {
+        author= "HC Aux Team"
+        requiredAddos[]=
+        {
+
+        };
+        weapons[]=
+        {
+
+        };
+        units[]=
+        {
+
+        };
+    };
+;}
+class CfgFactionClasses
+{
+	class HC_Faction
+	{
+		displayName="Horizon Company";
+	};
+};
+class CfgEditorCategories
+{
+	class HC_Props
+	{
+		displayName="Horizon Company. Supplies";
+	};
+};
+class CfgEditorSubcategories
+{
+	class HC_Resupply
+	{
+		displayName="Resupply";
+		priority=1;
+	};
+	class HC_VIP
+	{
+		displayName = "VIPs";
+	};
+};
+class CfgWeapons
+{
+
+};
+class CfgVehicles
+{
+
+};
