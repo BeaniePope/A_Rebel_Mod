716047e3b5343e4a4ceee0976bea58417bcbf9e9
BeaniePope <Swifter11110@gmail.com>
2024-07-16 16:49:13 -0400

    Ammobox

--- addons/hc_core/config.cpp
+++ addons/hc_core/config.cpp
@@ -57,8 +57,38 @@ class CfgWeapons
 			"hc_core\data\helmets\HC_commando_helmet_co.paa"
 		};
 	};
+
 };
 class CfgVehicles
 {
+	//Objects
+	class JLTS_Ammobox_weapons_GAR;
+	class HC_Ammobox_REB: JLTS_Ammobox_weapons_GAR
+	{
+		author = "Queen";
+		scope = 2;
+		displayName = "Ammobox Rebels [HC]";
+		hiddenSelections[]=
+		{
+			"camo2"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"hc_core\data\objects\screen_1_REB_weapons1_co"
+		};
+		icon="iconCrateWpns";
+		armor=1000;
+		class TransportMagazines
+		{
 
+		};
+		class TransportWeapons
+		{
+
+		};
+		class TransportItems
+		{
+
+		};
+	};
 };
