e11e79e6fc9865569607c5be25cb55a3df471809
BeaniePope <Swifter11110@gmail.com>
2024-07-16 22:52:31 -0400

    Spray Paint

--- addons/hc_core/config.cpp
+++ addons/hc_core/config.cpp
@@ -92,3 +92,18 @@ class CfgVehicles
 		};
 	};
 };
+
+class ACE_Tags {
+    class yourTagClass {
+        displayName = "Horizon";  // Name of your tag being displayed in the interaction menu
+        requiredItem = "ACE_SpraypaintRed";  // Required item to have in the inventory to be able to spray your tag (eg. `"ACE_SpraypaintBlack"`, `"ACE_SpraypaintRed"`, `"ACE_SpraypaintGreen"`, `"ACE_SpraypaintBlue"` or any custom item from `CfgWeapons`)
+        textures[] = 
+		{
+			"hc_core\data\logos\hclogospraypaint_co.paa"
+		
+		};  // List of texture variations (one is randomly selected when tagging)
+        //materials[] = {"path\to\material.rvmat"}; // Optional: List of material variations (one is randomly selected). Keep empty if you don't need a custom material.
+        icon = "hc_core\data\logos\hclogo.paa";  // Icon being displayed in the interaction menu
+        //tagModel = "UserTexture1m_F"; // Optional: The 3D Model that will be spawned with the texture on it, can either be CfgVehicles classname or P3D file path.
+    };
+};
