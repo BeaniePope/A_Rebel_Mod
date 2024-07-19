cdc36a35c9a2f8bd83ed2d634fc02b01a7a2127c
GregGregGregGreg1234 <129117638+GregGregGregGreg1234@users.noreply.github.com>
2024-07-19 01:42:36 +0100

    start if imps and moving stuff

--- addons/hc_core/config.cpp
+++ addons/hc_core/config.cpp
@@ -1,6 +1,6 @@
 class CfgPatches
 {
-    class hc_core
+    class HC_Core
     {
         author= "HC Aux Team"
         requiredAddos[]=
@@ -20,7 +20,7 @@ class CfgPatches
         };
     };
 };
-
+///idk why these here
 #include "uiDefines.hpp"
 #include "data\ui\hclr.hpp"
 
@@ -61,7 +61,7 @@ class CfgWeapons
 		displayName = "[HC] Commando Helmet (Endor)";
 		hiddenSelectionsTextures[]=
 		{
-			"hc_core\data\helmets\HC_commando_helmet_Endor_co.paa"
+			"HC_Core\data\helmets\HC_commando_helmet_Endor_co.paa"
 		};
 	};
 	class HC_Commando_Helmet_Cloud: HC_Commando_Helmet_Endor
@@ -69,7 +69,7 @@ class CfgWeapons
 		displayName = "[HC] Commando Helmet (Cloud)";
 		hiddenSelectionsTextures[]=
 		{
-			"hc_core\data\helmets\HC_commando_helmet_Cloud_co.paa"
+			"HC_Core\data\helmets\HC_commando_helmet_Cloud_co.paa"
 		};
 	};
 	class HC_Commando_Helmet_Desert: HC_Commando_Helmet_Endor
@@ -77,7 +77,7 @@ class CfgWeapons
 		displayName = "[HC] Commando Helmet (Green)";
 		hiddenSelectionsTextures[]=
 		{
-			"hc_core\data\helmets\HC_commando_helmet_Desert_co.paa"
+			"HC_Core\data\helmets\HC_commando_helmet_Desert_co.paa"
 		};
 	};
 	class HC_Commando_Helmet_Black: HC_Commando_Helmet_Endor
@@ -85,7 +85,7 @@ class CfgWeapons
 		displayName = "[HC] Commando Helmet (Black)";
 		hiddenSelectionsTextures[]=
 		{
-			"hc_core\data\helmets\HC_commando_helmet_Black_co.paa"
+			"HC_Core\data\helmets\HC_commando_helmet_Black_co.paa"
 		};
 	};
 
@@ -247,11 +247,11 @@ class CfgVehicles
     // 	tf_dialogUpdate = "[""CH%1""] call TFAR_fnc_updateLRDialogToChannel;";
 	// 	hiddenSelectionsTextures[] = 
 	// 	{
-	// 		"\hc_core\data\backpacks\HC_Rebel_Backpack_Black_co.paa"
+	// 		"\HC_Core\data\backpacks\HC_Rebel_Backpack_Black_co.paa"
 	// 	};
 	// 	hiddenSelectionsMaterials[] =
 	// 	{
-	// 		"\hc_core\data\HC_Rebel_Backpack_Black.rvmat"
+	// 		"\HC_Core\data\HC_Rebel_Backpack_Black.rvmat"
 	// 	};
 	// };
 	class JMSLLTE_back_rebradio_v1_black;
@@ -281,7 +281,7 @@ class CfgVehicles
 		};
 		hiddenSelectionsTextures[]=
 		{
-			"hc_core\data\objects\screen_1_REB_weapons1_co"
+			"HC_Core\data\objects\screen_1_REB_weapons1_co"
 		};
 		icon="iconCrateWpns";
 		armor=1000;
@@ -517,11 +517,11 @@ class ACE_Tags {
         requiredItem = "ACE_SpraypaintRed";  // Required item to have in the inventory to be able to spray your tag (eg. `"ACE_SpraypaintBlack"`, `"ACE_SpraypaintRed"`, `"ACE_SpraypaintGreen"`, `"ACE_SpraypaintBlue"` or any custom item from `CfgWeapons`)
         textures[] = 
 		{
-			"hc_core\data\logos\hclogospraypaint_co.paa"
+			"HC_Core\data\logos\hclogospraypaint_co.paa"
 		
 		};  // List of texture variations (one is randomly selected when tagging)
         //materials[] = {"path\to\material.rvmat"}; // Optional: List of material variations (one is randomly selected). Keep empty if you don't need a custom material.
-        icon = "hc_core\data\logos\hclogo.paa";  // Icon being displayed in the interaction menu
+        icon = "HC_Core\data\logos\hclogo.paa";  // Icon being displayed in the interaction menu
         //tagModel = "UserTexture1m_F"; // Optional: The 3D Model that will be spawned with the texture on it, can either be CfgVehicles classname or P3D file path.
     };
 };
